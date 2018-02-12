1b2dae85bde7a6d814881af7792db89b9ac47c86
Chris Turner <chris@pronghorn.tech>
2018-02-12 01:04:27 -0700

    simplified tests for await()

--- src/test/kotlin/tech/pronghorn/coroutines/awaitable/AwaitTests.kt
+++ src/test/kotlin/tech/pronghorn/coroutines/awaitable/AwaitTests.kt
@@ -18,117 +18,93 @@ package tech.pronghorn.coroutines.awaitable
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.RepeatedTest
+import tech.pronghorn.coroutines.awaitable.future.CoroutineFuture
 import tech.pronghorn.coroutines.core.CoroutineWorker
-import tech.pronghorn.coroutines.service.Service
-import tech.pronghorn.coroutines.service.SingleWriterExternalQueueService
+import tech.pronghorn.coroutines.core.Service
 import tech.pronghorn.test.*
-import java.nio.channels.SelectionKey
 import java.util.ArrayDeque
-import java.util.Queue
 
 class AwaitTests : PronghornTest() {
-    class AwaitingService(override val worker: AwaitingWorker,
-                          private val futures: List<InternalFuture<Int>>) : Service() {
-        suspend override fun run() {
+    // A service that awaits the provided futures and updates its worker's done variable
+    class AwaitingService(override val worker: CoroutineWorker,
+                          private val futures: List<CoroutineFuture<Int>>) : Service() {
+        @Volatile
+        var done = 0
+            private set
+
+        override suspend fun run() {
             when (futures.size) {
-                1 -> {
-                    worker.done = await(futures.first())
-                }
+                1 -> done = await(futures.first())
                 2 -> {
                     val (futureA, futureB) = futures
                     val (a, b) = await(futureA, futureB)
-                    worker.done = a + b
+                    done = a + b
                 }
                 3 -> {
                     val (futureA, futureB, futureC) = futures
                     val (a, b, c) = await(futureA, futureB, futureC)
-                    worker.done = a + b + c
+                    done = a + b + c
                 }
                 4 -> {
                     val (futureA, futureB, futureC, futureD) = futures
                     val (a, b, c, d) = await(futureA, futureB, futureC, futureD)
-                    worker.done = a + b + c + d
+                    done = a + b + c + d
                 }
-                else -> throw Exception("Awaiting five awaiters at once isn't currently supported.")
+                else -> throw Exception("Awaiting only supported for 1 to 4 awaiters.")
             }
         }
     }
 
-    class CompletingService(override val worker: AwaitingWorker,
-                            private val promises: Queue<InternalFuture.InternalPromise<Int>>) : SingleWriterExternalQueueService<Int>() {
-        suspend override fun process(work: Int) {
-            val promise = promises.poll()
-            promise.complete(work)
-        }
-    }
-
-    class AwaitingWorker(futureCount: Int) : CoroutineWorker() {
-        @Volatile
-        var done = 0
-
-        private val futures = List(futureCount, { InternalFuture<Int>() })
-        private val promises = ArrayDeque(futures.map(InternalFuture<Int>::promise))
-        private val completingService = CompletingService(this, promises)
-        val externalWriter = completingService.getQueueWriter()
-
-        override val services: List<Service> = listOf(
-                AwaitingService(this, futures),
-                completingService
-        )
-    }
-
+    // Creates a worker with a service to await and a service to complete a variable number of internal futures
     fun variableAwait(futureCount: Int) {
-        val worker = AwaitingWorker(futureCount)
+        val worker = CoroutineWorker()
+        val externalWorker = CoroutineWorker()
+        val futures = List(futureCount, { CoroutineFuture<Int>() })
+        val awaitingService = AwaitingService(worker, futures)
+        worker.addService(awaitingService)
         worker.start()
+        externalWorker.start()
+
+        var totalValue = 0
+        externalWorker.executeInWorker {
+            val promises = ArrayDeque(futures.map { future -> future.externalPromise(/*worker*/) })
+            var promise = promises.poll()
+            while(promise != null){
+                val value = random.nextInt(64)
+                promise.complete(value)
+                totalValue += value
+                promise = promises.poll()
+            }
+        }
 
         try {
-            assertEquals(0, worker.done)
-
-            var totalValue = 0
-            for (x in 1..futureCount) {
-                val value = random.nextInt(64)
-                totalValue += value
-                worker.externalWriter.offer(value)
-            }
-
             eventually {
-                assertEquals(totalValue, worker.done)
+                assertEquals(totalValue, awaitingService.done)
             }
         }
         finally {
             worker.shutdown()
+            externalWorker.shutdown()
         }
     }
 
-    /*
-     * Tests awaiting for one internal future utilizing the worker/services above
-     */
-    @RepeatedTest(repeatCount)
-    fun await1() {
+    @RepeatedTest(heavyRepeatCount)
+    fun await1Test() {
         variableAwait(1)
     }
 
-    /*
-     * Tests awaiting for two internal futures utilizing the worker/services above
-     */
-    @RepeatedTest(repeatCount)
-    fun await2() {
+    @RepeatedTest(heavyRepeatCount)
+    fun await2Test() {
         variableAwait(2)
     }
 
-    /*
-     * Tests awaiting for two internal futures utilizing the worker/services above
-     */
-    @RepeatedTest(repeatCount)
-    fun await3() {
+    @RepeatedTest(heavyRepeatCount)
+    fun await3Test() {
         variableAwait(3)
     }
 
-    /*
-     * Tests awaiting for two internal futures utilizing the worker/services above
-     */
-    @RepeatedTest(repeatCount)
-    fun await4() {
+    @RepeatedTest(heavyRepeatCount)
+    fun await4Test() {
         variableAwait(4)
     }
 }
